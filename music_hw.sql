31a097eb534a0be435d8222fee49758e08d71c81
Sanzhay Ayurov <ayurov2004@gmail.com>
2023-01-04 15:20:32 +0900

    Change music_hw and add gitignore

--- music_hw.sql
+++ music_hw.sql
@@ -4,23 +4,16 @@ create table if not exists genres(
 );
 
 
-create table if not exists genre_artist(
-	id serial primary key,
-	artist_id integer references artists(id),
-	genre_id integer references genres(id)
-);
-
-
 create table if not exists artists(
 	id serial primary key,
 	name varchar(40) not null
 );
 
 
-create table if not exists artist_album(
+create table if not exists genre_artist(
 	id serial primary key,
 	artist_id integer references artists(id),
-	album_id integer references albums(id)
+	genre_id integer references genres(id)
 );
 
 
@@ -31,18 +24,18 @@ create table if not exists albums(
 );
 
 
-create table if not exists tracks(
+create table if not exists artist_album(
 	id serial primary key,
-	name varchar(40) not null,
-	duration integer,
+	artist_id integer references artists(id),
 	album_id integer references albums(id)
 );
 
 
-create table if not exists tracks_collection(
+create table if not exists tracks(
 	id serial primary key,
-	track_id integer references tracks(id),
-	collection_id integer references collections(id)
+	name varchar(40) not null,
+	duration_min float,
+	album_id integer references albums(id)
 );
 
 
@@ -53,6 +46,11 @@ create table if not exists collections(
 );
 
 
+create table if not exists tracks_collection(
+	id serial primary key,
+	track_id integer references tracks(id),
+	collection_id integer references collections(id)
+);
 
 
 
